fe5b61e9ebd0cf34d0e67744e9cc04cbddd2c510
NinaDlvx <165060963+NinaDlvx@users.noreply.github.com>
2024-06-04 18:06:35 +0200

    Added API integration

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
-function generateQuote(event) {
-  event.preventDefault();
+function displayQuote(response) {
   new Typewriter("#quote", {
-    strings: `Either you run the day or the day runs you`,
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generateQuote(event) {
+  event.preventDefault();
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = `a20035dd93a6tbea3b9cff448a2b7eeo`;
+  let prompt = `Generate an English quote about ${instructionsInput.value}?`;
+  let context = `You're a philosophical AI assistant that can make up inspiring quotes. Always provide a quote in italics in a <p>, add an Author if it's known and sign of with <strong>Provided by SheCodes AI</strong>.`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayQuote);
 }
 
 let quoteFormElement = document.querySelector("#quote-generator-form");
